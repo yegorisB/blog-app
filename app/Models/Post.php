9741806586ac2e238511a70e9ff4d4c08e8009fb
yegoris <128933877+yegorisB@users.noreply.github.com>
2023-11-10 18:59:45 -0500

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'body', 'user_id'];
+    protected $fillable = ['title', 'body', 'user_id', 'image_path'];
     
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
