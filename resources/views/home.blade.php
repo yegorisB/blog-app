0ac1f3aa86c45f33af01e59c7a3e7ba814eefd42
yegoris <128933877+yegorisB@users.noreply.github.com>
2023-11-10 19:01:04 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,10 +16,15 @@ content="width=device-width, initial-scale=1.0">
     </form><br>
     <div style="border: 3px solid blue;">
         <h2>Create a new post</h2>
-        <form action="/create-post" method="POST">
+        <form action="/create-post" method="POST" enctype="multipart/form-data">
             @csrf  
             <input type="text" name="title" placeholder="post title" style="margin: 10px"/>
             <textarea name="body" placeholder="body content ..." style="margin: 10px"></textarea>
+            <div>
+                <label>Picture</label>
+                <input type="file" name="image"  />
+            </div>
+
             <button style="margin: 10px">Save Post</button>
           
         </form>
@@ -31,6 +36,7 @@ content="width=device-width, initial-scale=1.0">
         @foreach($posts as $post)
         <div style="background-color: rgb(176, 204, 230); padding: 10px; margin: 10px;">
             <h3>{{$post['title']}} by {{$post->user->name}}&nbsp;&nbsp;{{$post['updated_at']}} &nbsp UTC</h3>
+            <p><img src={{asset('images/'. $post->image_path)}} alt="" width=10% ></p>
             <p>{{$post['body']}}</p>
             <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
             <form action="/delete-post/{{$post->id}}" method="POST">
@@ -46,7 +52,7 @@ content="width=device-width, initial-scale=1.0">
     @else
     <div style="border: 3px solid blue;"  >
          <h2 style="margin: 10px">Register</h2>
-        <form action="/register" method="POST">  
+        <form action="/register" method="POST" >  
             @csrf  
             <input name="name" type="text" placeholder="name" style="margin: 10px">
             <input name="email" type="text" placeholder="email" style="margin: 10px">
@@ -68,6 +74,7 @@ content="width=device-width, initial-scale=1.0">
         @foreach($posts as $post)
         <div style="background-color: rgb(204, 176, 230); padding: 10px; margin: 10px;">
             <h3>{{$post['title']}} by {{$post->user->name}}&nbsp;&nbsp;{{$post['updated_at']}} &nbsp UTC </h3>
+            <img class="center" src={{asset('images/'. $post->image_path)}} alt="" width=10% >
             <p>{{$post['body']}}</p>
 
         </div>
