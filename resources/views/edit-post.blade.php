97df12bdf29abba0b705e94f05fa5cddd2c5f8cd
yegoris <128933877+yegorisB@users.noreply.github.com>
2023-11-10 19:00:40 -0500

    Update edit-post.blade.php

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -8,11 +8,15 @@
 </head>
 <body>
     <h1>Edit Post<h1>
-    <form action="/edit-post/{{$post->id}}" method="POST">
+    <form action="/edit-post/{{$post->id}}" method="POST" enctype="multipart/form-data">
     @csrf
     @method('PUT')
     <input type="text" name="title" value="{{$post->title}}">
     <textarea name="body">{{$post->body}}</textarea>
+    <div>
+        <label>Picture</label>
+        <input type="file" name="image"  />
+    </div>
     <button>Save Changes</button>
 
     </form>
